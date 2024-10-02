a086e65eed11bcb87ef8611b28b4d5cfa5e1f05c
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-10-02 14:38:12 +0300

    development of the project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+<<<<<<< HEAD
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { FooterComponent } from './common-ui/footer/footer.component';
 import { HeaderComponent } from './common-ui/header/header.component';
 
+=======
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+>>>>>>> 2320c6fa6be4a0894d2a834067025cafe2e8d22d
 
 @Component({
   selector: 'app-root',
   standalone: true,
+<<<<<<< HEAD
   imports: [RouterOutlet,FooterComponent, HeaderComponent],
+=======
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+>>>>>>> 2320c6fa6be4a0894d2a834067025cafe2e8d22d
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
