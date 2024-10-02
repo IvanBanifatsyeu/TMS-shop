c32712c0c82fc5838460516ff0dc2e734434c805
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-10-02 14:58:32 +0300

    development of the project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { FooterComponent } from './common-ui/footer/footer.component';
-import { HeaderComponent } from './common-ui/header/header.component';
 
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'TMS-shop';
