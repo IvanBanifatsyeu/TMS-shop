3c546e137ceba2546b40a92dfa24ca6c64cda472
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-10-06 12:18:15 +0300

    installed ngx-translate lib and configured the app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-
+import {TranslateModule, TranslateLoader} from "@ngx-translate/core";
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDDn_kO_3PhEIRjdgtkrmHlW7e3rFrM12c',
@@ -14,11 +16,22 @@ const firebaseConfig = {
   appId: '1:248115805872:web:853f08d9f5ac5f14da4575',
 };
 
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
+  new TranslateHttpLoader(http, './i18n/', '.json');
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideHttpClient(),
     provideFirestore(() => getFirestore()),
+    importProvidersFrom([TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+    })])
   ],
 };
