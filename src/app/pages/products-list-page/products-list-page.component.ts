77c57b464f6c82452c95df556512ee96e0a6a655
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-10-08 22:02:39 +0300

    made intro section

--- src/app/pages/products-list-page/products-list-page.component.ts
+++ src/app/pages/products-list-page/products-list-page.component.ts
@@ -1,13 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-products-list-page',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './products-list-page.component.html',
   styleUrl: './products-list-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductsListPageComponent {
-
+  translate = inject(TranslateService);
 }
