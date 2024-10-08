3073b842c409c5e2ff5322adbbde4c6af2cc42fc
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-10-08 17:49:45 +0300

    implemented position of header depending on current route

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,12 +3,18 @@ import {
   Component,
   HostBinding,
   inject,
+  OnDestroy,
+  OnInit,
+  signal,
 } from '@angular/core';
 import { NavigationComponent } from '../navigation/navigation.component';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { AccountPopupComponent } from './account-popup/account-popup.component';
 import { CommonModule } from '@angular/common';
 import { trigger, transition, style, animate } from '@angular/animations';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, Subscription, tap } from 'rxjs';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-header',
@@ -18,6 +24,7 @@ import { trigger, transition, style, animate } from '@angular/animations';
     TranslateModule,
     AccountPopupComponent,
     CommonModule,
+    SvgIconComponent,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
@@ -26,19 +33,44 @@ import { trigger, transition, style, animate } from '@angular/animations';
     trigger('fadeInOut', [
       transition(':enter', [
         style({ opacity: 0 }),
-        animate('300ms ease-in', style({ opacity: 1 }))
+        animate('300ms ease-in', style({ opacity: 1 })),
       ]),
-      transition(':leave', [
-        animate('300ms ease-out', style({ opacity: 0 }))
-      ])
-    ])
-  ]
+      transition(':leave', [animate('300ms ease-out', style({ opacity: 0 }))]),
+    ]),
+  ],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   @HostBinding('class.position-absolute') isAbsolute = true;
   isPopupVisible = false;
   translate = inject(TranslateService);
-  
+  currentRoute = signal<string>(''); 
+  subscription!: Subscription;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.subscription = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.currentRoute.set(event.url); // Обновляем сигнал при изменении маршрута
+        this.handleRouteChange(event.url); // Обрабатываем изменение маршрута
+      });
+  }
+
+  private handleRouteChange(url: string): void {
+    // добавляем/удаляем класс в зависимости от текущего роута
+    if (url === '/') {
+      this.isAbsolute = true;
+    } else if (url === '/shop') {
+      this.isAbsolute = false;
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 
   showPopup() {
     this.isPopupVisible = true;
