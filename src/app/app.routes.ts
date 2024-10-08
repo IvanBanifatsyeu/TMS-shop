702c2ce275f79b4630ae352374d1415cd536c69a
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-10-08 15:51:38 +0300

    added routes for navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
 import {  HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductsListPageComponent } from './pages/products-list-page/products-list-page.component';
 
-export const routes: Routes = [{ path: '', component: HomePageComponent }];
+export const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'shop', component: ProductsListPageComponent },
+
+];
--- src/app/shared/components/navigation/navigation.component.ts
+++ src/app/shared/components/navigation/navigation.component.ts
@@ -1,16 +1,20 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, RouterModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavigationComponent {
-  navItems: string[] = ['NAVIGATION.HOME', 'NAVIGATION.SHOP'];
+  navItems: { text: string, path: string }[] = [
+    { text: 'NAVIGATION.HOME',path : '' },
+    { text: 'NAVIGATION.SHOP',path : 'shop' },
+  ];
   translate = inject(TranslateService);
 }
