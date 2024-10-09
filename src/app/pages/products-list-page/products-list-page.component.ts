e093e1afc003f22e26dd99480fa61762bf57d692
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-10-09 13:08:16 +0300

    created main structure of page, added svg for switching style of layout

--- src/app/pages/products-list-page/products-list-page.component.ts
+++ src/app/pages/products-list-page/products-list-page.component.ts
@@ -1,14 +1,31 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { SvgIconComponent } from '../../shared/components/svg-icon/svg-icon.component';
+import { CommonModule } from '@angular/common';
+import { ProductFirebaseService } from '../../core/services/product-firebase.service';
+import { Product } from '../../core/interfaces/product.interface';
+import { ProductCardComponent } from "../../shared/components/product-card/product-card.component";
 
 @Component({
   selector: 'app-products-list-page',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, SvgIconComponent, CommonModule, ProductCardComponent],
   templateUrl: './products-list-page.component.html',
   styleUrl: './products-list-page.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductsListPageComponent {
+export class ProductsListPageComponent implements OnInit {
   translate = inject(TranslateService);
+  productsFirebaseService = inject(ProductFirebaseService);
+  productsList: Product[] | null = null;
+  
+  ngOnInit(): void {
+    this.productsFirebaseService.getProducts().subscribe((res) => {
+      this.productsList = res
+    })
+  }
+
+
+
+
 }
