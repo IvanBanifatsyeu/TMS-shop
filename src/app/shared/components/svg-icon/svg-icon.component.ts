1f8535f5d743fa27057d7b4152fcd6a7420ceece
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-11-19 17:28:19 +0300

    fix bag with img path for deploy

--- src/app/shared/components/svg-icon/svg-icon.component.ts
+++ src/app/shared/components/svg-icon/svg-icon.component.ts
@@ -1,19 +1,23 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'svg[icon]',
   standalone: true,
   imports: [],
   template: '<svg:use [attr.href]="href"></svg:use>',
-  styles: [`
-    
-  `],
-   changeDetection: ChangeDetectionStrategy.OnPush
+  styles: [``],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SvgIconComponent {
+export class SvgIconComponent implements OnInit {
   @Input() icon = '';
+  href!: string;
 
-  get href() {
-    return `/assets/svg/${this.icon}.svg#${this.icon}`;
+  ngOnInit() {
+    this.href = `assets/svg/${this.icon}.svg#${this.icon}`;
   }
 }
